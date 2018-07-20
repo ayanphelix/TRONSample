1f30022145abc0b745c5836f1f737791d8746022
Ayanbola Felix <ayanbolafelix@ayanbolas-macbook-pro.local>
2018-07-20 01:17:08 +0100

    Initial Commit

---
+++ TRONSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRONSample
+//
+//  Created by Ayanbola Felix on 20/07/2018.
+//  Copyright © 2018 Ayanbola Felix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
